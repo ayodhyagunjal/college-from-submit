8f81813d6341485700e026838afb11b3b3d816eb
Ayodhya <ayodhyashelke22@gmail.com>
2020-12-18 16:15:21 +0530

    changes

--- class.php
+++ class.php
@@ -3,7 +3,7 @@ $con = mysqli_connect('localhost','root');
 mysqli_select_db($con,'fromdb');
 
 $nameid = $_POST['datapost'];
-
+// changs
 $q = "select * from classes where mid = '$nameid'";
 $result = mysqli_query($con,$q);
 while($rows = mysqli_fetch_array($result)){
